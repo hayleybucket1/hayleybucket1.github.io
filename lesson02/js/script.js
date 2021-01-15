5782cc7dab95194b09209a51821e0f0f1aa00a69
hayleybucket1 <72775372+hayleybucket1@users.noreply.github.com>
2021-01-15 15:19:19 -0600

    update design principles

---
+++ lesson02/js/script.js
@@ -0,0 +1,6 @@
+// Last Update
+var date = document.lastModified; 
+document.getElementById("lastUpdate").textContent = "Last Updated: " + date;
+// Year
+const year = new Date();
+document.getElementById("copyYear").textContent = year.getFullYear();
