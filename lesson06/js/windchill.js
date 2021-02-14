98f22c1eda4882545dfcdeb6d937d65b9bdc66ba
hayleybucket1 <hayleybucke1@gmail.com>
2021-02-13 18:25:39 -0600

    fix windchill

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -2,13 +2,14 @@
  *Processing: calculate wind chill
  *Output: display wind chill*/
 
-const tempNum = parseFlat(document.getElementById("temp").textContent);
-console.log(tempNumber);
+const tempNum = parseFloat(document.getElementById("temp").textContent);
+console.log(tempNum);
 
 const speedNum = parseFloat(document.getElementById("speed").textContent);
 console.log(speedNum);
 
 var windchill = 35.74 + (0.6215 * tempNum) - (35.75 * Math.pow(speedNum, 0.16)) + (0.4275 * tempNum * Math.pow(speedNum, 0.16));
+windchill = Math.round(windchill);
 
 if (tempNum <= 50 && speedNum > 3) {
     document.getElementById("chill").textContent = "Wind Chill: " + windchill + "\xB0F";
